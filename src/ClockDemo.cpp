9e5d6421624eaed5d2c0fd0f8e506589eaefdfb0
alifeee <alifeeerenn@hotmail.com>
2024-03-14 21:23:01 +0000

    update text to scroll around screen

--- src/ClockDemo.cpp
+++ src/ClockDemo.cpp
@@ -4,8 +4,17 @@
 #include <Adafruit_GFX.h>
 #include <BigClock.h>
 
+#define MAX_X_DEVIATION 36
+#define MAX_Y_DEVIATION 9
+
 GFXcanvas1 *canvas = NULL;
 BigClock *bc = NULL;
+
+uint8_t scroll = 0;
+int reverse = 0;
+
+uint8_t scrolly = 0;
+int reversey = 0;
 
 void setup()
 {
@@ -16,21 +25,34 @@ void setup()
 
 void loop()
 {
-  displayGFX();
-}
+  if (scroll > MAX_X_DEVIATION)
+    reverse = 1;
+  else if (scroll < 1)
+    reverse = 0;
 
-void displayGFX()
-{
+  if (scrolly > MAX_Y_DEVIATION)
+    reversey = 1;
+  else if (scrolly < 1)
+    reversey = 0;
+
+  if (reverse)
+    scroll--;
+  else
+    scroll++;
+
+  if (reversey)
+    scrolly--;
+  else
+    scrolly++;
+
   canvas->fillScreen(0);
-  canvas->setCursor(1, 1);
+  canvas->setCursor(1 + scroll, 1 + scrolly);
   canvas->print("Sheffield");
-  canvas->setCursor(37, 9);
-  canvas->print("Bonkspace");
+  canvas->setCursor(37 - scroll, 9 + scrolly);
+  canvas->print("Hackspace");
 
   uint8_t *buffer = canvas->getBuffer();
 
   bc->output(buffer);
   bc->output(buffer); // Output twice otherwise it doesn't update the whole screen.
-
-  delay(10000);
 }
