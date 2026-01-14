b054468ad752f7eb1d9ad145ec226ea249fce170
Adam Kuczynski <a+github@typedef.io>
2026-01-14 16:20:07 +0000

    set bw pixels throughout the loop

--- src/main-checkerboard.cpp
+++ src/main-checkerboard.cpp
@@ -39,16 +39,12 @@ void loop()
     // read the microswitch
     // int val = digitalRead(SWITCH_PIN);
 
-    canvas->fillScreen(0);
     // checkerboard pattern (every x/y odd/even pixel is on/off)
     for (int i = 0; i < MAX_X; i++)
     {
         for (int j = 0; j < MAX_Y; j++)
         {
-            if ((i + j) % 2 == 0)
-            {
-                canvas->drawPixel(i, j, 1);
-            }
+            canvas->drawPixel(i, j, (i + j) % 2);
         }
     }
 
