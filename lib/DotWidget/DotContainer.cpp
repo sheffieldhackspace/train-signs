ac7fc28c1c773da10b44362d17949236615e3f17
Adam Kuczynski <github@typedef.io>
2026-02-28 08:43:03 +0000

    First version of DotContainer

---
+++ lib/DotWidget/DotContainer.cpp
@@ -0,0 +1,51 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2026 Adam Kuczyński
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "DotContainer.h"
+
+void DotHorizontal::add(DotElement *element) {
+  _elements.push_back(element);
+  _width += element->width();
+  _height = max(_height, element->height());
+}
+
+void DotHorizontal::draw(int16_t x, int16_t y) const {
+  for (const auto *element: _elements) {
+    element->draw(x, y);
+    x += element->width();
+  }
+}
+
+void DotVertical::add(DotElement *element) {
+  _elements.push_back(element);
+  _width = max(_width, element->width());
+  _height += element->height();
+}
+
+void DotVertical::draw(int16_t x, int16_t y) const {
+  for (const auto *element: _elements) {
+    element->draw(x, y);
+    y += element->height();
+  }
+}
---
+++ lib/DotWidget/DotContainer.h
@@ -0,0 +1,63 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2026 Adam Kuczyński
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#pragma once
+
+#include <Adafruit_GFX.h>
+#include <vector>
+#include "DotElement.h"
+
+class DotHorizontal : public DotElement {
+public:
+  explicit DotHorizontal(Adafruit_GFX *canvas) : _canvas(canvas), _width(0), _height(0) {}
+
+  void add(DotElement *element);
+  void draw(int16_t x, int16_t y) const override;
+  [[nodiscard]] uint16_t width() const override { return _width; }
+  [[nodiscard]] uint16_t height() const override { return _height; }
+
+private:
+  Adafruit_GFX *_canvas;
+  std::vector<DotElement *> _elements;
+  uint16_t _width;
+  uint16_t _height;
+};
+
+class DotVertical : public DotElement {
+public:
+  explicit DotVertical(Adafruit_GFX *canvas) : _canvas(canvas), _width(0), _height(0) {}
+
+  void add(DotElement *element);
+  void draw(int16_t x, int16_t y) const override;
+  [[nodiscard]] uint16_t width() const override { return _width; }
+  [[nodiscard]] uint16_t height() const override { return _height; }
+
+private:
+  Adafruit_GFX *_canvas;
+  std::vector<DotElement *> _elements;
+  uint16_t _width;
+  uint16_t _height;
+};
